148c4cbcbb715cbf6ec80b6d33bc506b253fb745
lunafard <129810877+lunafard@users.noreply.github.com>
2023-04-19 01:38:59 -0400

    Update xxxxxxxxxxxxxxxxxxxxxxxxxxxx.lua

--- xxxxxxxxxxxxxxxxxxxxxxxxxxxx.lua
+++ xxxxxxxxxxxxxxxxxxxxxxxxxxxx.lua
@@ -1,16 +1,16 @@
 local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Nofitication")
 if not GUI then
-    local STX_Nofitication = Instance.new("ScreenGui")
-    local STX_NofiticationUIListLayout = Instance.new("UIListLayout")
-    STX_Nofitication.Name = "STX_Nofitication"
-    STX_Nofitication.Parent = game.CoreGui
-    STX_Nofitication.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    STX_Nofitication.ResetOnSpawn = false
+    local DNS_Nofitication = Instance.new("ScreenGui")
+    local DNS_NofiticationUIListLayout = Instance.new("UIListLayout")
+    DNS_Nofitication.Name = "DNS_Nofitication"
+    DNS_Nofitication.Parent = game.CoreGui
+    DNS_Nofitication.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    DNS_Nofitication.ResetOnSpawn = false
     
-    STX_NofiticationUIListLayout.Name = "STX_NofiticationUIListLayout"
-    STX_NofiticationUIListLayout.Parent = STX_Nofitication
-    STX_NofiticationUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
-    STX_NofiticationUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-    STX_NofiticationUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
+    DNS_NofiticationUIListLayout.Name = "DNS_NofiticationUIListLayout"
+    DNS_NofiticationUIListLayout.Parent = DNS_Nofitication
+    DNS_NofiticationUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
+    DNS_NofiticationUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+    DNS_NofiticationUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
 else
 end
