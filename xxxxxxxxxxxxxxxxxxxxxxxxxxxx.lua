0f4673e17027559ef66a93c0132463c7d216fddc
lunafard <129810877+lunafard@users.noreply.github.com>
2023-04-25 02:18:43 -0400

    Update xxxxxxxxxxxxxxxxxxxxxxxxxxxx.lua

--- xxxxxxxxxxxxxxxxxxxxxxxxxxxx.lua
+++ xxxxxxxxxxxxxxxxxxxxxxxxxxxx.lua
@@ -1,16 +1,35 @@
-local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Nofitication")
+if getgenv().Demise.General.Intro then
+getgenv().Get =
+    setmetatable(
+    {},
+    {
+        __index = function(A, B)
+            return game:GetService(B)
+        end
+    }
+)
+local CoreGui = Get.CoreGui
+local GUI = CoreGui:FindFirstChild("MAW_NOTIFICATION")
 if not GUI then
-    local DNS_Nofitication = Instance.new("ScreenGui")
-    local DNS_NofiticationUIListLayout = Instance.new("UIListLayout")
-    DNS_Nofitication.Name = "MAW_NOTIFICATION"
-    DNS_Nofitication.Parent = game.CoreGui
-    DNS_Nofitication.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    DNS_Nofitication.ResetOnSpawn = false
+    local MAW_NOTIFICATION = Instance.new("ScreenGui")
+    if syn and syn.protect_gui then
+    syn.protect_gui(MAW_NOTIFICATION)
+    MAW_NOTIFICATION.Parent = CoreGui
+    elseif gethui then
+    MAW_NOTIFICATION.Parent = gethui()
+    else
+    MAW_NOTIFICATION.Parent = CoreGui
+    end
+    local MAW_NOTIFICATIONUIListLayout = Instance.new("UIListLayout")
+    MAW_NOTIFICATION.Name = "MAW_NOTIFICATION"
+    MAW_NOTIFICATION.Parent = game.CoreGui
+    MAW_NOTIFICATION.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    MAW_NOTIFICATION.ResetOnSpawn = false
     
-    DNS_NofiticationUIListLayout.Name = "MAW_NOTIFICATIONUIListLayout"
-    DNS_NofiticationUIListLayout.Parent = MAW_NOTIFICATION
-    DNS_NofiticationUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
-    DNS_NofiticationUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-    DNS_NofiticationUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
+    MAW_NOTIFICATIONUIListLayout.Name = "MAW_NOTIFICATIONUIListLayout"
+    MAW_NOTIFICATIONUIListLayout.Parent = MAW_NOTIFICATION
+    MAW_NOTIFICATIONUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
+    MAW_NOTIFICATIONUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+    MAW_NOTIFICATIONUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
 else
 end
