1ff6abb798211e411b33dd34861050d8bbc52ed9
lunafard <129810877+lunafard@users.noreply.github.com>
2023-04-25 01:34:57 -0400

    Update xxxxxxxxxxxxxxxxxxxxxxxxxxxx.lua

--- xxxxxxxxxxxxxxxxxxxxxxxxxxxx.lua
+++ xxxxxxxxxxxxxxxxxxxxxxxxxxxx.lua
@@ -2,13 +2,13 @@ local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Nofitication")
 if not GUI then
     local DNS_Nofitication = Instance.new("ScreenGui")
     local DNS_NofiticationUIListLayout = Instance.new("UIListLayout")
-    DNS_Nofitication.Name = "DNS_Notification"
+    DNS_Nofitication.Name = "MAW_NOTIFICATION"
     DNS_Nofitication.Parent = game.CoreGui
     DNS_Nofitication.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
     DNS_Nofitication.ResetOnSpawn = false
     
-    DNS_NofiticationUIListLayout.Name = "DNS_NotificationUIListLayout"
-    DNS_NofiticationUIListLayout.Parent = DNS_Notification
+    DNS_NofiticationUIListLayout.Name = "MAW_NOTIFICATIONUIListLayout"
+    DNS_NofiticationUIListLayout.Parent = MAW_NOTIFICATION
     DNS_NofiticationUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
     DNS_NofiticationUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
     DNS_NofiticationUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
